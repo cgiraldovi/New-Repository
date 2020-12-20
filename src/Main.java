e3709b6b9c44f6755f2454cc0eced133e47aa17d
C.giraldo <cgiraldo@Unal.edu.co>
2020-12-20 16:19:26 -0500

    Este el primer commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Este es un mensaje");
+        System.out.println("Este es un segundno mensaje");
+    }
+}
