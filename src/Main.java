36f7cb386935abb176f82df816da1c90e2420802
C.giraldo <cgiraldo@Unal.edu.co>
2020-12-21 20:33:35 -0500

    Este es el primer ejercicio de la seccion de constructores

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main (String[] args){
-        System.out.println("Este es un mensaje");
-        System.out.println("Este es un segundno mensaje");
-    }
-}
