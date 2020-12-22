017dd33e3fae7a120b9ab4cc522041977f4d4923
C.giraldo <cgiraldo@Unal.edu.co>
2020-12-21 21:18:38 -0500

    Este es el tercer ejercicio de la seccion de constructores

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,13 +1,24 @@
 public class Passenger {
     private int checkedBags;
     private int freeBags;
-    //getters and setters elided
     private double perBagFee;
+
+    //Se ha agregado un constructor dentro de otro constructor
+
+    public Passenger (int freeBags){
+        this(freeBags > 1 ? 25.0d : 50.0d);
+        this.freeBags = freeBags;
+    }
+
+    public Passenger (int freeBags, int checkedBags){
+        this(freeBags);
+        this.checkedBags = checkedBags;
+    }
 
     public void setCheckedBags(int i) {
     }
 
-    public Passenger (int freeBags){
-        this.freeBags = freeBags;
+    public Passenger (double perBagFee){
+        this.perBagFee = perBagFee;
     }
 }
