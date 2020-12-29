b72bfaf08a00fc88220b12cc252c90402dd9746d
C.giraldo <cgiraldo@Unal.edu.co>
2020-12-29 15:37:22 -0500

    Este es un segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Este es un mensaje");
-        System.out.println("Este es un segundno mensaje");
+    public static void main(String[] args){
+        CargoFlight vuelo1 = new CargoFlight();
+        Flight[] listaDeAviones = new Flight[5];
+        listaDeAviones[0] = new Flight();
+        listaDeAviones[1] = new Flight();
+        listaDeAviones[2] = new CargoFlight();
+
+
+        vuelo1.sumarPeso();
+        listaDeAviones[0].sumarPeso();
+        listaDeAviones[2].sumarPeso();
+        
     }
 }
